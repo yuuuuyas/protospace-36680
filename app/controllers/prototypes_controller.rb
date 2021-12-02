17687d2610ac6a6797a4a331d0adb82e1c23b748
yuuuuyas <yuyasakurako0110@gmail.com>
2021-12-02 15:34:17 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,10 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
-    @prototype = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
@@ -17,30 +19,29 @@ class PrototypesController < ApplicationController
       render :new
     end
   end
-  
+
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
-  
+
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to prototype_path(@prototype.id)
+      redirect_to prototype_path(@prototype)
     else
       render :edit
-    end    
-  end  
+    end
+  end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
-    @prototype.destroy
-    redirect_to root_path
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
@@ -49,9 +50,13 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in? && current_user.id == @user
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    unless user_signed_in? && current_user == @prototype.user
       redirect_to action: :index
     end
-  end  
+  end
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -6,5 +6,5 @@ class Prototype < ApplicationRecord
   validates :concept, presence: true
   validates :image, presence: true
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
